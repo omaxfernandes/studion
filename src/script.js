b860fe0c8f74e314a3fbdc181d05a836b1927774
maxsatos <max.sfernandes@outlook.com>
2023-08-12 18:13:20 -0300

    feat (mobile, tablet and Desktop): added responsiveness to the home page

--- src/script.js
+++ src/script.js
@@ -1,30 +1,61 @@
 $(document).ready(() => {
-    const slickOptions = {
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: false,
-        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>',
-        nextArrow:'<button type="button" class="slick-next slider__next-arrow">Previous</button>',
-    };
-  $('.slider').slick(slickOptions);
+  // MENU
+  $(".navbar__menu-btn").on("click", function () {
+    $(".navbar__links").toggleClass("active");
+    $(this).find("i").toggleClass("fa-bars");
+    $(this).find("i").toggleClass("fa-times");
+  });
 
-  $('.footer__form-button').on('click', () => {
-    const email = $('#email').val();
-    Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "username",
-    Password : "password",
-    To : 'them@website.com',
-    From : email,
-    Subject : "Please add me to the newsletter",
-    Body : `Hello, 
-    
-    I would like to be added to the site's newsletter.
-    My email is ${email}.
-    
-    Thanks!`
-}).then(
-  message => alert(message)
-);
+  // CAROUSEL
+  const slickOptions = {
+    autoplay: true,
+    dots: false,
+    // pauseOnHover: false,
+    prevArrow:
+      '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+    nextArrow:
+      '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
+  };
+
+  $(".slider").slick(slickOptions);
+
+  const counterOptions = {
+    delay: 10,
+    time: 1000,
+  };
+
+  setInterval(() => {
+    const numbersCollection = document.querySelectorAll(".counter__number");
+
+    numbersCollection.forEach((number) => {
+      const currentNumber = parseInt(number.innerHTML);
+      number.innerHTML = currentNumber + 1;
+    });
+  }, 1000);
+
+  $(".counter__number").counterUp(counterOptions);
+
+  $(".footer__form-button").on("click", () => {
+    const email = $("#email").val();
+
+    const emailOptions = {
+      Host: "smtp.yourisp.com",
+      Username: "username",
+      Password: "password",
+      To: "contato@studion.com",
+      From: email,
+      Subject: "Por favor me adicione na newsletter",
+      Body: `Olá,
+      
+      Eu gostaria de ser adicionado na newsletter do site.
+      Meu email é ${email}.
+      
+      Obrigado!
+      `,
+    };
+
+    Email.send(emailOptions).then((message) => {
+      alert(message);
+    });
   });
 });
